4513c947731ab3d2a9cf688d5d56c144cd0d133c
junskii <arjuna.andio@binus.ac.id>
2025-09-30 00:20:56 +0700

    fixes

--- resources/views/shuttle/booking.blade.php
+++ resources/views/shuttle/booking.blade.php
@@ -1,9 +1,22 @@
-@extends('layouts.mobile')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('title', 'Pemesanan Shuttle - MyTalenta')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Pemesanan Shuttle - MyTalenta</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
 
-@section('content')
-    <div class="min-h-screen">
-        @livewire('shuttle.shuttle-booking')
-    </div>
-@endsection
+<body class="bg-gray-50 font-inter">
+    @livewire('shuttle.shuttle-booking')
+    @livewireScripts
+</body>
+
+</html>
